ad446a7905517b550af9e879489e04d77283cc1f
IsaacGarcia <desarrollo.backend@rinorisk.com>
2025-04-02 14:45:40 -0700

    d

---
+++ logic.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    const titulo = document.getElementById("tituloInvitado");
+    if (titulo) {
+      titulo.textContent = `Invitación #${id}`;
+    }
+    const numeroPases = document.getElementById("numeroPases");
+    if (numeroPases) {
+      numeroPases.textContent = "2 Personas";
+    }
+
+    document.getElementById("aceptarInvitacionBtn").addEventListener("click", () => {
+        document.getElementById("tituloInvitado").textContent = "Xd!";
+      });
+  });
+  
+  
+  
