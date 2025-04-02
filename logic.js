c4dad0c10f15f9d645dcc6a0deb4c42669cf6795
IsaacGarcia <desarrollo.backend@rinorisk.com>
2025-04-02 15:10:47 -0700

    s

--- logic.js
+++ logic.js
@@ -1,9 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
     const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+    const id = params.get("id");
     const titulo = document.getElementById("tituloInvitado");
     if (titulo) {
-      titulo.textContent = `Invitación #${id}`;
+      if (id) {
+        titulo.textContent = `Invitación #${id}`;
+      }else{
+        titulo.textContent = "¡Bienvenido!";
+      }
     }
     const numeroPases = document.getElementById("numeroPases");
     if (numeroPases) {
